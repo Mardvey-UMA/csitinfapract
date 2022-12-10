1b9a4d7dcf1c4fd9bdabb2b25aef4f53cad6df7e
Mattew Razinov <83042309+Mardvey-UMA@users.noreply.github.com>
2022-12-10 20:18:11 +0400

    Update simple_version.cpp
    simple_version v0.0.2

--- simple_version.cpp
+++ simple_version.cpp
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 //Глобальные переменные//
@@ -9,16 +11,24 @@ string black_pix = "\033[30;40m  \033[m";
 string white_pix = "\033[37;47m  \033[m";
 string red_pix = "\033[31;41m  \033[m";
 string blue_pix = "\033[34;44m  \033[m";
-string alphavile = "*abcdefgh*";
+
+string num_pix = "\033[32;42m  \033[m";
+string letter_pix = "\033[33;43m  \033[m";
+string empty_pix = "\033[36;46m  \033[m";
+
+string alphavile = "*ABCDEFGH*";
 string numbers = "*87654321*";
 //////////////////////////////////////
 
+void Sleep(int x){
+    std::this_thread::sleep_for(std::chrono::milliseconds(x));
+}
+
+void setcp(char naprav, int pix){
     //    \033[#A передвинуть курсор вверх на # строк
     //    \033[#B передвинуть курсор вниз на # строк
     //    \033[#С передвинуть курсор вправо на # столбцов
     //    \033[#D передвинуть курсор влево на # столбцов
-
-void setcp(char naprav, int pix){
     string c = "\033[" + to_string(pix) + naprav;
     cout << c;
 }
@@ -53,29 +63,69 @@ void paint_black(char color){
     setcp('D',6);
     cout << black_pix << black_pix << black_pix;
     setcp('A',2);
-} 
+}
+
+void paint_number_block(char number){
+    cout << num_pix << num_pix << num_pix;
+    setcp('B',1);
+    setcp('D',6);
+    cout << num_pix << "\033[1;37m" << number << "\033[m" << " " << num_pix;
+    setcp('B',1);
+    setcp('D',6);
+    cout << num_pix << num_pix << num_pix;
+    setcp('A',2);
+}
+
+void paint_letter_block(char letter){
+    cout << letter_pix << letter_pix << letter_pix;
+    setcp('B',1);
+    setcp('D',6);
+    cout << letter_pix << letter << ' ' << letter_pix;
+    setcp('B',1);
+    setcp('D',6);
+    cout << letter_pix << letter_pix << letter_pix;
+    setcp('A',2);
+}
+void paint_empty_block(){
+    cout << empty_pix << empty_pix << empty_pix;
+    setcp('B',1);
+    setcp('D',6);
+    cout << empty_pix << empty_pix << empty_pix;
+    setcp('B',1);
+    setcp('D',6);
+    cout << empty_pix << empty_pix << empty_pix;
+    setcp('A',2);
+}
 
 void out_put_pole(char **pole){
     system("clear");
-    for (int i = 1; i < 9; i++){
-        for (int j = 1; j < 9; j++){
-            if ((i + j) % 2 == 0)
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            if ((j == 0 || j == 9) && (i != 0 && i != 9)){
+                paint_number_block(pole[i][j]);
+            }else if ((i == 0 || i == 9) && (j != 0 && j != 9)){
+                paint_letter_block(pole[i][j]);
+            }else if (pole[i][j] == '*'){
+                paint_empty_block();
+            }else if ((i + j) % 2 == 0){
                 paint_white(pole[i][j]);
-            else
+            }else{
                 paint_black(pole[i][j]);
+            }                
         }
-        cout << "\n\n\n";
-
+        setcp('B',3);
+        setcp('D',60);
     }
+    setcp('A',48);
+    //cout << "##########";
 }
 
-void set_figure(char **pole, char color, int x, int y){
-    if (x > 8 || y > 8)
-        cout << "error";
-    pole[x][y] = color;
+void set_figure(char **pole, string start_pos, string fin_pos){
+    
 }
 
 int main(){
+system("clear");
 char **pole = new char*[10];
 for (int k = 0; k < 10; k++)
     pole[k] = new char [10];
@@ -101,7 +151,15 @@ for (int k = 0; k < 10; k++)
         }
     }
 ///////////////////////////////////////
-
+//setcp('B',5);
+//setcp('C',10);
+//paint_number_block('1');
+//paint_letter_block('A');
 out_put_pole(pole);
-cout << "\n\n\n\n\n\n\n";
+Sleep(3000);
+//set_figure(pole, 'N', 5, 4);
+Sleep(500);
+//set_figure(pole, 'R', 5, 4);
+out_put_pole(pole);
+cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 }
