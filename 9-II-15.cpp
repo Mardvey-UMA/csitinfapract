ef8e87f0eeef57da963b8f98416ce63d3afaff32
Mattew Razinov <83042309+Mardvey-UMA@users.noreply.github.com>
2022-12-23 09:10:34 +0400

    Update 9-II-15.cpp

--- 9-II-15.cpp
+++ 9-II-15.cpp
@@ -10,8 +10,7 @@ int main(){
     string s, s1;
     int x, x1;
     while (in.peek() != EOF){
-        getline(in, s);
-        getline(in, s1);
+        in >> s >> s1;
         x = stoi(s);
         x1 = stoi(s1);
         out << min(x, x1) << endl;
