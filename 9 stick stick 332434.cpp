3c9c16d1dc19c13b00eb42b143de1458371a8d72
Mattew Razinov <83042309+Mardvey-UMA@users.noreply.github.com>
2022-12-27 16:37:27 +0400

    Add files via upload

---
+++ 9 stick stick 332434.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+ifstream in("input.txt");
+ofstream out("output.txt");
+
+int main() {
+	string s1, s2;
+	double x1, x2;
+	while (in.peek() != EOF) {
+		in >> s1 >> s2;
+		x1 = stod(s1);
+		x2 = stod(s2);
+		out << (x1 + x2) / 2. << endl;
+	}
+	in.close();
+	out.close();
+	
+}
