37f924bc8628ed5de315e8abbf0c9d5211667ac0
Mattew Razinov <83042309+Mardvey-UMA@users.noreply.github.com>
2022-12-27 16:31:15 +0400

    Add files via upload

---
+++ 9 stick 5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+ifstream in("input.txt");
+
+int main() {
+	string s;
+	unsigned int max_len = 0, pos, cnt = 1;
+	while (getline(in, s)) {
+		if (s.size() > max_len) {
+			max_len = s.size();
+			pos = cnt;
+		}
+		cnt++;
+	}
+	cout << pos;
+	
+}
