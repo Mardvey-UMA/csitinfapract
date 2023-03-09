96078f30ccca6e1cb4d9830cb22f33a4a31acfcb
Mattew Razinov <83042309+Mardvey-UMA@users.noreply.github.com>
2023-03-09 19:59:44 +0400

    Update string_IV-19.cpp

--- metodichka_2_sem/string_IV-19.cpp
+++ metodichka_2_sem/string_IV-19.cpp
@@ -10,28 +10,38 @@ bool is_digit(char c){
 }
 
 bool is_sybmol(char c){
-    if (c == 'e' || c == '.' || c == ',' || c == '-')
+    if (c == 'e' || c == '.' || c == ',' || c == '-' || c == '+')
         return true;
     return false; 
 }
 
-int main(){
-    string s;
+bool is_float(string s){
     bool flag = true;
-    getline(cin, s);
+    if ((s.size() == 1 && is_sybmol(s[0]) == true))
+        if (is_digit(s[0]) == false)
+            flag = false;
 
-    if (s[0] == '-')
+    if ((s.size() == 2 && is_sybmol(s[0]) == true && is_sybmol(s[1]) == true))
+        if (is_digit(s[0]) == false || is_digit(s[1]) == false)
+            flag = false;
+
+    if (s[0] == '-' || s[0] == '+')
         s[0] = '1';
     
-    int cnt_p = 0, cnt_e = 0, e_pos, m_pos;
+    int cnt_p = 0, cnt_e = 0, e_pos, m_pos, p_pos;
 
     e_pos = s.find('e');
     m_pos = s.find('-');
+    p_pos = s.find('+');
 
-    if (e_pos == -1 && m_pos != -1)
+    if ((e_pos == -1 && m_pos != -1) || (e_pos == -1 && p_pos != -1))
         flag = false;
-    else if (e_pos != -1 && m_pos != -1){
-        if (not(m_pos - e_pos == 1)){
+    else if ((e_pos != -1 && m_pos != -1)){
+        if ((not(m_pos - e_pos == 1))){
+            flag = false;
+        }
+    }else if ((e_pos != -1 && m_pos != -1)){
+        if ((not(p_pos - e_pos == 1))){
             flag = false;
         }
     }
@@ -62,18 +72,18 @@ int main(){
         for (int i = 0; i < s.size(); i++)
             flag2 *= (is_digit(s[i]) || is_sybmol(s[i]));
     }
+
     if ((flag && flag2) == true)
+        return true;
+    else
+        return false;
+}
+
+int main(){
+    string s;
+    getline(cin, s);
+    if (is_float(s))
         cout << "Правильное десятичное число" << endl;
     else
         cout << "Неправильное десятичное число" << endl;
 }
-
-//test
-//input
-//1e-100 Правильное десятичное число
-//0. Правильное десятичное число
-//-10e-10 Правильное десятичное число
-//--1000e-1000 Неправильное десятичное число
-// 5. Правильное десятичное число
-// 103e45 Правильное десятичное число
-// aaae-gggg0 Неправильное десятичное число
